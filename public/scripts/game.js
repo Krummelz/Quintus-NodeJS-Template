71d4f61d52f12bee04e7ba48c6f1d4b86447ffa4
Jacques Wihann Dés Prés <exabyte1@gmail.com>
2014-01-09 15:48:03 +0200

    Initial Commit

---
+++ public/scripts/game.js
@@ -0,0 +1,13 @@
+var Q = Quintus()
+    .include("Sprites, Scenes, Input, 2D, Touch, UI")
+    .setup({
+        maximize: true
+    });
+
+Q.scene("", function (stage) {
+
+});
+
+Q.load("", function () {
+
+});
